fc7db01ddf1042510a43a119f4fb4fabb062b8b7
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-07-04 19:50:14 -0300

    criptografando senha

--- src/main/java/com/api/service/ColaboradorService.java
+++ src/main/java/com/api/service/ColaboradorService.java
@@ -3,6 +3,7 @@ package com.api.service;
 import com.api.model.ColaboradorModel;
 import com.api.repository.ColaboradorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,9 +15,14 @@ public class ColaboradorService {
 
     @Autowired
     private ColaboradorRepository colaboradorRepository;
+    
+        @Autowired
+    private BCryptPasswordEncoder encoder;
 
     public ColaboradorModel salvar(ColaboradorModel colaboradorModel) {
-
+        
+        String senhaCriptografada = encoder.encode(colaboradorModel.getSenha());
+        colaboradorModel.setSenha(senhaCriptografada);
         return colaboradorRepository.save(colaboradorModel);
     }
 }
--- src/main/java/com/api/service/GestorService.java
+++ src/main/java/com/api/service/GestorService.java
@@ -3,6 +3,7 @@ package com.api.service;
 import com.api.model.GestorModel;
 import com.api.repository.GestorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,8 +16,13 @@ public class GestorService {
     @Autowired
     private GestorRepository gestorRepository;
     
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+    
     public GestorModel salvar(GestorModel gestorModel) {
         
+        String senhaCriptografada = encoder.encode(gestorModel.getSenha());
+        gestorModel.setSenha(senhaCriptografada);
         return gestorRepository.save(gestorModel);
     }
 }
