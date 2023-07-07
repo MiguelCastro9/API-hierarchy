e2b83e45f3840ca5b07d74e011604a7d0a1883f7
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-07-06 22:29:52 -0300

    ajustes e requicoes javascript

--- API-web/script.js
+++ API-web/script.js
@@ -1,3 +1,9 @@
+function atualizarPagina() {
+    location.reload();
+}
+
+document.getElementById("atualizar").addEventListener("click", atualizarPagina);
+
 document.getElementById("salvarGestor").addEventListener("click", function() {
     var nome = document.getElementById("gestor").value;
     var senha = document.getElementById("senhaGestor").value;
@@ -24,33 +30,121 @@ document.getElementById("salvarGestor").addEventListener("click", function() {
     xhr.send(jsonData);
 });
 
-document.getElementById("salvarColaborador").addEventListener("click", function() {
-    var nome = document.getElementById("colaborador").value;
-    var senha = document.getElementById("senhaColaborador").value;
-
+function getGestorId() {
+    var selectElement = document.getElementById("selectGestor");
+    var gestorId = selectElement.value;
+    return gestorId;
+  }
+  
+  function salvarColaborador() {
+    var nomeColaborador = document.getElementById("colaborador").value;
+    var senhaColaborador = document.getElementById("senhaColaborador").value;
+    var gestorId = getGestorId();
+  
     var data = {
-        nome: nome,
-        senha: senha,
-        gestor: {
-            id: id,
-            nome: nome,
-            senha: senha
-        }
+      nome: nomeColaborador,
+      senha: senhaColaborador,
+      gestor: {
+        id: gestorId
+      }
     };
-
+  
     var jsonData = JSON.stringify(data);
-
+  
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/colaborador/salvar", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onload = function() {
-        if (xhr.status === 200) {
-            console.log("Requisição bem-sucedida!");
-            alert('salvo com sucesso!')
-            console.log(xhr.responseText);
-        } else {
-            console.log("Ocorreu um erro na requisição. Código: " + xhr.status);
-        }
+      if (xhr.status === 200) {
+        console.log("Requisição bem-sucedida!");
+        alert('Salvo com sucesso!');
+        console.log(xhr.responseText);
+      } else {
+        console.log("Ocorreu um erro na requisição. Código: " + xhr.status);
+      }
     };
     xhr.send(jsonData);
-});
+  }
+
+document.getElementById("salvarColaborador").addEventListener("click", salvarColaborador);
+
+function listarGestores() {
+    fetch("http://localhost:8080/gestor/listar")
+        .then(response => response.json())
+        .then(gestores => {
+            console.log("Requisição bem-sucedida!");
+            var tabela = document.getElementById("tabelaGestores");
+            var tbody = tabela.getElementsByTagName("tbody")[0];
+            tbody.innerHTML = "";
+
+            gestores.forEach(gestor => {
+                var nome = gestor.nome;
+                var score_senha = gestor.score_senha;
+
+                var row = tbody.insertRow();
+                var cellNome = row.insertCell(0);
+                var cellScore_senha = row.insertCell(1);
+
+                cellNome.innerHTML = nome;
+                cellScore_senha.innerHTML = score_senha;
+            });
+        })
+        .catch(error => {
+            console.log("Ocorreu um erro na requisição: ", error);
+        });
+}
+
+listarGestores();
+
+function listarGestoresNoSelect() {
+    var selectElement = document.getElementById("selectGestor");
+  
+    fetch("http://localhost:8080/gestor/listar")
+      .then(response => response.json())
+      .then(gestores => {
+        console.log("Requisição bem-sucedida!");
+  
+        gestores.forEach(gestor => {
+          var option = document.createElement("option");
+          option.value = gestor.id;
+          option.textContent = gestor.nome;
+          selectElement.appendChild(option);
+        });
+      })
+      .catch(error => {
+        console.log("Ocorreu um erro na requisição: ", error);
+      });
+  }
+  
+listarGestoresNoSelect();
+
+function listarColaboradores() {
+    fetch("http://localhost:8080/colaborador/listar")
+      .then(response => response.json())
+      .then(colaboradores => {
+        console.log("Requisição bem-sucedida!");
+        var tabela = document.getElementById("tabelaColaboradores");
+        var tbody = tabela.getElementsByTagName("tbody")[0];
+        tbody.innerHTML = "";
+  
+        colaboradores.forEach(colaborador => {
+          var nome = colaborador.nome;
+          var score_senha = colaborador.score_senha;
+          var gestor = colaborador.gestor.nome;
+  
+          var row = tbody.insertRow();
+          var cellNome = row.insertCell(0);
+          var cellScoreSenha = row.insertCell(1);
+          var cellGestor = row.insertCell(2);
+  
+          cellNome.innerHTML = nome;
+          cellScoreSenha.innerHTML = score_senha;
+          cellGestor.innerHTML = gestor;
+        });
+      })
+      .catch(error => {
+        console.log("Ocorreu um erro na requisição: ", error);
+      });
+  }
+
+  listarColaboradores();
